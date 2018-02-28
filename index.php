ce7c38963946b53d45c796c988e78bd7fa9f5b89
jkmalan <jkmalan@admincmd.com>
2018-02-28 13:54:15 -0500

    [ADD] Create basic navigation bar

--- index.php
+++ index.php
@@ -6,17 +6,23 @@
  * All other rights reserved.
  */
 
-$page_style = "";
-$page_title = "";
-?>
+require_once './php/initialize.php';
 
+$page_style = "index.css";
+$page_title = "Home";
+?>
 <html>
 
 <?php include ROOT_DIR . 'php/template/head_template.php'; ?>
 
 <body>
 
+<?php include ROOT_DIR . 'php/template/navigation_template.php'; ?>
 
+
+
+<?php include ROOT_DIR . 'php/template/footer_template.php'; ?>
 
 </body>
+
 </html>
---
+++ php/template/footer_template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2017 John Malandrakis
+ * This software is provided to St. John's University to be
+ * used, modified, and distributed at their discretion.
+ * All other rights reserved.
+ */
+?>
+    <footer>
+
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
