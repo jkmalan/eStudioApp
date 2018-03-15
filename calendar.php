099a23e38cda55f48a6a313ae41c154cc46007dd
jkmalan <jkmalan@admincmd.com>
2018-03-15 16:18:40 -0400

    [MOD] Include and test bootstrap plugins

--- calendar.php
+++ calendar.php
@@ -20,10 +20,27 @@ $page_title = "Calendar";
 <?php include ROOT_DIR . 'php/template/navigation_template.php'; ?>
 
     <main>
-
+        <div class="container">
+            <div id="calendar"></div>
+        </div>
     </main>
 
 <?php include ROOT_DIR . 'php/template/footer_template.php'; ?>
+
+<script type="text/javascript">
+    var calendar = $("#calendar").calendar({
+        tmpl_path: "/tmpls/",
+        view: 'week',
+        events_source: [{
+            "id": 293,
+            "title": "Event 1",
+            "url": "http://example.com",
+            "class": "event-important",
+            "start": 1521143344531, // Milliseconds
+            "end": 1521143353875 // Milliseconds
+        }]
+    });
+</script>
 
 </body>
 
--- php/template/footer_template.php
+++ php/template/footer_template.php
@@ -14,5 +14,7 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.0-beta/js/bootstrap-select.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-calendar/0.2.5/js/calendar.min.js"></script>
