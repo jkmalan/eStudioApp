99daba7da620eb780f4e05617199e74bdcf6df62
jkmalan <jkmalan@admincmd.com>
2018-05-08 15:08:09 -0400

    [ADD] Build out scheduler landing page UI and redirects

--- scheduler.php
+++ scheduler.php
@@ -11,7 +11,24 @@ require_once './php/initialize.php';
 $page_style = "scheduler.css";
 $page_title = "Scheduler";
 
-
+$time_start = $time_end = "";
+$camp_code = $bldg_code = $room_code = "";
+if (isset($_GET['submit'])) {
+    $search = filter_input(INPUT_GET, 'search');
+    if ($search === "rooms") {
+        $camp_code = filter_input(INPUT_GET, 'camp');
+        $bldg_code = filter_input(INPUT_GET, 'bldg');
+        $room_code = filter_input(INPUT_GET, 'room');
+        header('Location: ' . BASE_URL . 'search_rooms.php?camp=' . $camp_code . '&bldg=' . $bldg_code . '&room=' . $room_code . '&submit=Submit');
+        exit();
+    } else if ($search === "times") {
+        $time_start = filter_input(INPUT_GET, 'time-start');
+        $time_end = filter_input(INPUT_GET, 'time-end');
+        header('Location: ' . BASE_URL . 'search_times.php?time-start=' . $time_start . '&time-end=' . $time_end . '&submit=Submit');
+        exit();
+    }
+    $events = searchEBR($camp_code, $bldg_code, $room_code);
+}
 
 ?>
 <html>
@@ -42,25 +59,25 @@ $page_title = "Scheduler";
                     <input type="hidden" name="search" value="rooms">
                     <div class="row" id="search-rooms">
                         <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="camp-input">Select Campus</label>
+                            <label for="camp">Select Campus</label>
                             <div>
-                                <select class="form-control" name="camp-input">
+                                <select class="form-control" name="camp">
 
                                 </select>
                             </div>
                         </div>
                         <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="bldg-input">Select Building</label>
+                            <label for="bldg">Select Building</label>
                             <div>
-                                <select class="form-control" name="bldg-input">
+                                <select class="form-control" name="bldg">
 
                                 </select>
                             </div>
                         </div>
                         <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="room-input">Select Room</label>
+                            <label for="room">Select Room</label>
                             <div>
-                                <select class="form-control" name="room-input">
+                                <select class="form-control" name="room">
 
                                 </select>
                             </div>
@@ -68,21 +85,15 @@ $page_title = "Scheduler";
                     </div>
                     <div class="row" id="search-times">
                         <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="date-input">Select Date</label>
+                            <label for="time-start">Select Start Time</label>
                             <div>
-                                <input class="form-control" type="date" name="date-input" value="">
+                                <input class="form-control" type="datetime-local" name="time-start" value="">
                             </div>
                         </div>
                         <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="time-input">Select Time</label>
+                            <label for="time-end">Select End Time</label>
                             <div>
-                                <input class="form-control" type="time" name="time-input" value="">
-                            </div>
-                        </div>
-                        <div class="form-group col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                            <label for="datetime-input">Select Time</label>
-                            <div>
-                                <input class="form-control" type="datetime-local" name="datetime-input" value="">
+                                <input class="form-control" type="datetime-local" name="time-end" value="">
                             </div>
                         </div>
                     </div>
@@ -104,7 +115,8 @@ $page_title = "Scheduler";
                             </div>
                         </div>
                     </div>
-                    <button class="btn btn-primary col-xs-4 col-xs-offset-4 col-sm-4 col-sm-offset-4" type="submit">Search</button>
+                    <input type="text" name="search" value="rooms" hidden>
+                    <input class="btn btn-primary col-xs-4 col-xs-offset-4 col-sm-4 col-sm-offset-4" type="submit" name="submit">
                 </form>
             </div>
         </div>
@@ -153,11 +165,11 @@ $page_title = "Scheduler";
                 $('input[name=search]').val("courses");
             });
 
-            let campInput = $("select[name=camp-input]");
-            let bldgInput = $("select[name=bldg-input]");
-            let roomInput = $("select[name=room-input]");
-            let subjInput = $("select[name=subj-input]");
-            let crseInput = $("select[name=crse-input]");
+            let campInput = $("select[name=camp]");
+            let bldgInput = $("select[name=bldg]");
+            let roomInput = $("select[name=room]");
+            let subjInput = $("select[name=subj]");
+            let crseInput = $("select[name=crse]");
 
             $(function() {
                 campInput.load("php/functions.php?ftype=camp");
@@ -165,16 +177,16 @@ $page_title = "Scheduler";
             });
 
             campInput.on('change', function () {
-                bldgInput.load("php/functions.php?ftype=bldg&campus=" + campInput.val());
-                roomInput.load("php/functions.php?ftype=room&building=" + bldgInput.val());
+                bldgInput.load("php/functions.php?ftype=bldg&camp=" + campInput.val());
+                roomInput.load("php/functions.php?ftype=room&bldg=" + bldgInput.val());
             }).trigger("change");
 
             bldgInput.on('change', function () {
-                roomInput.load("php/functions.php?ftype=room&building=" + bldgInput.val());
+                roomInput.load("php/functions.php?ftype=room&bldg=" + bldgInput.val());
             }).trigger("change");
 
             subjInput.on('change', function () {
-                crseInput.load("php/functions.php?ftype=crse&subject=" + subjInput.val());
+                crseInput.load("php/functions.php?ftype=crse&subj=" + subjInput.val());
             }).trigger("change");
         });
     </script>
